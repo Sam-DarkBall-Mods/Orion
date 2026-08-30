cac8266e6a90f5d02c751f1f1abf1c90583f85cc
Владимир <124524424+DarkBall123@users.noreply.github.com>
2026-08-30 21:15:27 +0300

    Restore Workshop mod icons (#9)

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,9 @@
 name = "Orion 1.0.0";
+picture = "logo128.paa";
+logo = "logo128.paa";
+logoOver = "logo128.paa";
+logoSmall = "logo64.paa";
+overviewPicture = "logo128.paa";
 actionName = "GitHub";
 action = "https://github.com/Sam-DarkBall-Mods/Orion";
 description = "Orion - Version 1.0.0";
